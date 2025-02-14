63e7f96754048e0862387fe1d0319bbb8bd5883e
Joe Anderson <freonite@gmail.com>
2025-02-13 21:19:53 -0600

    Added webview and functionality

--- app/src/main/java/com/corgrimm/metamaskinterview/MainActivity.kt
+++ app/src/main/java/com/corgrimm/metamaskinterview/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.corgrimm.metamaskinterview
 
+import android.content.Context
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.internal.ViewUtils.hideKeyboard
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    private lateinit var edtAddress: EditText
+    private lateinit var btnGo: Button
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +36,93 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        webView = findViewById(R.id.webview)
+        edtAddress = findViewById(R.id.edt_address)
+        btnGo = findViewById(R.id.btn_go)
+        progressBar = findViewById(R.id.progressBar)
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                progressBar.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                progressBar.visibility = View.GONE
+            }
+
+            override fun onReceivedError(
+                view: WebView?,
+                request: WebResourceRequest?,
+                error: WebResourceError?
+            ) {
+                super.onReceivedError(view, request, error)
+
+                Toast.makeText(
+                    this@MainActivity,
+                    "Error loading page: ${error?.description}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        webView.settings.javaScriptEnabled = true
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    finish()
+                }
+            }
+        })
+
+        btnGo.setOnClickListener {
+            var url = edtAddress.text.toString()
+            if (url.isNotEmpty()) {
+                webView.loadUrl(formatUrl(url))
+
+                // Hide the keyboard
+                hideKeyboard(edtAddress)
+            }
+        }
+
+        edtAddress.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_GO) {
+                webView.loadUrl(formatUrl(edtAddress.text.toString().trim()))
+                hideKeyboard(edtAddress)
+                true
+            } else {
+                false
+            }
+        }
+
+        if (savedInstanceState != null) {
+            webView.restoreState(savedInstanceState)
+        }
+        else {
+            webView.loadUrl("https://www.metamask.io")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        webView.saveState(outState)
+    }
+
+    fun formatUrl(url: String) : String{
+        var newUrl = url
+        if (!url.contains("http")) {
+            newUrl = "https://" + url
+            edtAddress.setText(newUrl)
+        }
+        return newUrl.lowercase()
+    }
+
+    fun hideKeyboard(view: View) {
+        val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+        view.clearFocus()
     }
 }
